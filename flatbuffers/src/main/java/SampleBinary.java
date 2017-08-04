7676d1036549fd482d55aa538330d955a539a511
Judd Gaddie <judd.gaddie@transficc.com>
2017-08-04 17:29:11 +0100

    working flatbuffers sample

--- flatbuffers/src/main/java/SampleBinary.java
+++ flatbuffers/src/main/java/SampleBinary.java
@@ -21,6 +21,8 @@ import com.transficc.MyGame.Sample.*;
 
 import java.nio.ByteBuffer;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 class SampleBinary {
     // Example how to use FlatBuffers to create and read binary buffers.
     public static void main(String[] args) {
@@ -69,32 +71,32 @@ class SampleBinary {
         Monster monster = Monster.getRootAsMonster(buf);
 
         // Note: We did not set the `mana` field explicitly, so we get back the default value.
-        assert monster.mana() == (short) 150;
-        assert monster.hp() == (short) 300;
-        assert monster.name().equals("Orc");
-        assert monster.color() == Color.Red;
-        assert monster.pos().x() == 1.0f;
-        assert monster.pos().y() == 2.0f;
-        assert monster.pos().z() == 3.0f;
+        assertThat(monster.mana()).isEqualTo((short) 150);
+        assertThat(monster.hp()).isEqualTo((short) 300);
+        assertThat(monster.name()).isEqualTo("Orc");
+        assertThat(monster.color()).isEqualTo(Color.Red);
+        assertThat(monster.pos().x()).isEqualTo(1.0f);
+        assertThat(monster.pos().y()).isEqualTo(2.0f);
+        assertThat(monster.pos().z()).isEqualTo(3.0f);
 
         // Get and test the `inventory` FlatBuffer `vector`.
         for (int i = 0; i < monster.inventoryLength(); i++) {
-            assert monster.inventory(i) == (byte) i;
+            assertThat(monster.inventory(i)).isEqualTo((byte) i);
         }
 
         // Get and test the `weapons` FlatBuffer `vector` of `table`s.
         String[] expectedWeaponNames = {"Sword", "Axe"};
         int[] expectedWeaponDamages = {3, 5};
         for (int i = 0; i < monster.weaponsLength(); i++) {
-            assert monster.weapons(i).name().equals(expectedWeaponNames[i]);
-            assert monster.weapons(i).damage() == expectedWeaponDamages[i];
+            assertThat(monster.weapons(i).name()).isEqualTo(expectedWeaponNames[i]);
+            assertThat(monster.weapons(i).damage()).isEqualTo((short)expectedWeaponDamages[i]);
         }
 
         // Get and test the `equipped` FlatBuffer `union`.
-        assert monster.equippedType() == Equipment.Weapon;
+        assertThat(monster.equippedType()).isEqualTo(Equipment.Weapon);
         Weapon equipped = (Weapon) monster.equipped(new Weapon());
-        assert equipped.name().equals("Axe");
-        assert equipped.damage() == 5;
+        assertThat(equipped.name()).isEqualTo("Axe");
+        assertThat(equipped.damage()).isEqualTo((short)5);
 
         System.out.println("The FlatBuffer was successfully created and verified!");
     }
