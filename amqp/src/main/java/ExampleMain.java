3ec995157a8070a6f7844de6aa038d8e8b039529
Judd Gaddie <judd.gaddie@transficc.com>
2017-08-09 17:48:34 +0100

    create amqp module

---
+++ amqp/src/main/java/ExampleMain.java
@@ -0,0 +1,9 @@
+import org.apache.qpid.proton.Proton;
+import org.apache.qpid.proton.engine.Collector;
+import org.apache.qpid.proton.engine.Connection;
+
+public class ExampleMain {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ amqp/src/main/java/Receiver.java
@@ -0,0 +1,12 @@
+import org.apache.qpid.proton.engine.BaseHandler;
+import org.apache.qpid.proton.engine.Event;
+
+public class Receiver extends BaseHandler {
+
+    @Override
+    public void onConnectionInit(Event e) {
+        super.onConnectionInit(e);
+    }
+
+
+}
